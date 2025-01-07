80be14abab0eca86d05a727a51204533136667bd
peam091044 <pongsagorn0910@gmail.com>
2025-01-07 09:44:18 +0700

    edit host run

--- webhook.py
+++ webhook.py
@@ -160,5 +160,7 @@ def webhook():
         return jsonify({"status": "error", "message": "An error occurred"}), 500
 
 
-if __name__ == "__main__":
-    app.run(port=5000)
+if __name__ == '__main__':
+    # ใช้ตัวแปร PORT หากมี หรือค่าเริ่มต้นเป็น 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
